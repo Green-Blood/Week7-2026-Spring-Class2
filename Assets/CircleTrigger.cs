8a89f56b93e57e7c0bb09a776ff152e114e3fc3c
Green-Blood <bloodjey98@gmail.com>
2026-03-13 14:28:16 +0500

    Update: Initial stuff from last week

---
+++ Assets/CircleTrigger.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class CircleTrigger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Hey i have changed smth
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        
+    }
+}
